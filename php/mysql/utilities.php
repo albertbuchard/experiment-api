c4e29f697d00f28712674fa6bf64cafdb7dac31b
albert.buchard <antinabi87>
2017-06-14 16:00:48 +0200

    Config

--- php/mysql/utilities.php
+++ php/mysql/utilities.php
@@ -1,11 +1,13 @@
 <?php
 if (file_exists('./utilities/configDev.php')) {
   require_once "./utilities/configDev.php";
+} elseif (file_exists('./utilities/config.dev.php')) {
+  require_once "./utilities/config.dev.php";
 } else {
   require_once "./utilities/config.php";
 }
 
 require_once "./utilities/types.php";
 require_once "./utilities/db.php";
-require_once "./utilities/rest.php"; 
+require_once "./utilities/rest.php";
 require_once "./utilities/session.php";
